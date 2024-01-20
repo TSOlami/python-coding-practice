7e0c8c9aa8a41e13a8d2cf2e8f3a2e021da20a75
TSOlami <tijani.saheed@yahoo.com>
2024-01-20 17:01:21 +0100

    Refactor two_sums function and update variable names

--- 1-pcp/two_sums.py
+++ 1-pcp/two_sums.py
@@ -4,23 +4,29 @@
 
 def two_sums(self, nums: List[int], target: int) -> List[int]:
 	""" Function to find the two numbers in a list that add up to a target number. """
-	
+
 	# Initiate a dictionary to store the numbers
-	num_dic = {}
+	num_dict = {}
+
 	# Loop through the list of numbers
 	for i, num in enumerate(nums):
+
 		# Find the complement of the current number
 		complement = target - num
+
 		# Check if the complement is in the dictionary
-		if complement in num_dic:
+		if complement in num_dict:
+
 			# Return the indices of the two numbers
-			return ([i, num_dic[complement]])
+			return ([i, num_dict[complement]])
+
 		# Add the current number to the dictionary
-		elif num not in num_dic:
-			num_dic[num] = i
+		elif num not in num_dict:
+			num_dict[num] = i
+
 	# Return an empty list if no two numbers add up to the target
     return([])
-		
+
         
         
 
